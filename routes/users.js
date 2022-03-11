bf0918d97b7ec5cff8492493165ea8269499252f
HeritageCollege <1896768@cegep-heritage.qc.ca>
2022-03-11 12:37:13 -0500

    users template

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,22 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const db = require('../model/db');
+const usersModel = require('../model/user');
+
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', async function (req, res) {
+
+  let result = await usersModel.getUsers();
+  res.render("users", { title: "Users", users: result });
+});
+
+router.get('/:userId', async function (req, res) {
+
+  let userId = parseInt(req.params.userId.trim());
+  let result = await usersModel.getUser(userId);
+  //console.log(result);
+  res.send(result);
 });
 
 module.exports = router;
